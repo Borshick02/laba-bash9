49ba771345bde25caef4864739c22e553d1709ea
Angelina Zolotova <aazolotova_1@edu.hse.ru>
2024-02-12 11:26:44 +0000

    Update var9.sh
    add 'help' for users' understanding

--- var9.sh
+++ var9.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
+    #!/bin/bash
 
-echo "Your number:"
-read length
-echo "Your number:"
-read width
-if ! [[ $length =~ ^[0-9]+$ ]] || ! [[ $width =~ ^[0-9]+$ ]]; then
-echo "error: wrong format"
-exit 1
-fi
-if ((length <= 0)) || ((width <= 0)); then
-echo "error: wrong format"
-exit 1
-fi
-perimeter=$((2*length + 2*width))
-echo "Your Perimeter is: $perimeter"
+    if [[ $1 == "--help" ]]; then
+    echo "This script calculates the perimeter of a rectangle."
+    echo "Usage: ./perimeter.sh [length] [width]"
+    exit 0
+    fi
 
+    echo "Your Length:"
+    read length
+    echo "Your Width:"
+    read width
+    if ! [[ $length =~ ^[0-9]+$ ]] || ! [[ $width =~ ^[0-9]+$ ]]; then
+    echo "error: wrong format"
+    exit 1
+    fi
+    if ((length <= 0)) || ((width <= 0)); then
+    echo "error: wrong format"
+    exit 1
+    fi
+    perimeter=$((2*length + 2*width))
+    echo "Your Perimeter is: $perimeter"
